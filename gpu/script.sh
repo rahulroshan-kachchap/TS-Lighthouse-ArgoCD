776a04daac7bdc66869e394b1fa4f69b536e7fb9
rahulroshan-kachchap <rahul.kachchap@hpe.com>
2022-02-03 19:54:31 +0530

    added changes

---
+++ gpu/script.sh
@@ -0,0 +1,7 @@
+cd /work
+git clone https://github.com/rahulroshan-kachchap/TS-Lighthouse-ArgoCD.git
+cd TS-Lighthouse-ArgoCD
+cp gpu/ansible_files/test.sh /work/
+cd /work
+ansible-playbook playbook.yaml -i host.ini > ansible.log
+
