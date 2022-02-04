0f8feeabcb36b0a36ee3380df7e5d2589ad1170c
rahulroshan-kachchap <rahul.kachchap@hpe.com>
2022-02-04 16:20:02 +0530

    added changes

--- gpu/script.sh
+++ gpu/script.sh
@@ -1,47 +1,54 @@
 sshpass()
 {
-user='rahul'
-pass='Password!234'
-ip='192.168.0.123'
-ssh-keygen -t rsa << HERE
+	user='rahul'
+	pass='Password!234'
+	ip='192.168.0.123'
+	ssh-keygen -t rsa << HERE
 
 
 
 
 HERE
-echo "ssh-keygen generated successfully"
-sleep 10s
-sshpass -p $pass ssh-copy-id -o StrictHostKeyChecking=no $user@$ip
-if [ $? -eq 0 ];
-then
-	echo "copy worked"
-else
-	echo "copy failed"
-fi
-echo "copy public key done"
-sleep 5s
+	echo "ssh-keygen generated successfully"
+	sleep 10s
+	sshpass -p $pass ssh-copy-id -o StrictHostKeyChecking=no $user@$ip
+	if [ $? -eq 0 ];
+	then
+		echo "copy worked"
+	else
+		echo "copy failed"
+	fi
+	echo "copy public key done"
+	sleep 5s
 }
+
 github()
 {
-cd /work
-git clone https://github.com/rahulroshan-kachchap/TS-Lighthouse-ArgoCD.git
-sleep 5s
-cd TS-Lighthouse-ArgoCD
-cp gpu/ansible_files/upgrade.sh /work/
+	cd /work
+	git clone https://github.com/rahulroshan-kachchap/TS-Lighthouse-ArgoCD.git
+	sleep 5s
+	cd TS-Lighthouse-ArgoCD
+	cp gpu/ansible_files/upgrade.sh /work/
 }
-run()
+
+runansible()
 {
-cd /work
-ansible-playbook --ssh-common-args="-o StrictHostKeyChecking=no" playbook.yaml -i host.ini >> ansible.log
+	cd /work
+	ansible-playbook --ssh-common-args="-o StrictHostKeyChecking=no" playbook.yaml -i host.ini >> ansible.log
 }
+
 runforever()
 {
 	wait
 }
+
 ########## MAIN ########
+
 sshpass
 
 github
+
+runansible
 
 runforever
 
