53761fe45e8754a030348a0f4d904d1abd74baaf
rahulroshan-kachchap <rahul.kachchap@hpe.com>
2022-02-04 12:45:13 +0530

    added changes

--- gpu/script.sh
+++ gpu/script.sh
@@ -1,12 +1,12 @@
+ssh-keygen -t rsa -N '' <<<''
+ssh-copy-id rahul@192.168.0.123 << HERE
+Password!234
+HERE
 cd /work
 git clone https://github.com/rahulroshan-kachchap/TS-Lighthouse-ArgoCD.git
 cd TS-Lighthouse-ArgoCD
 cp gpu/ansible_files/upgrade.sh /work/
 cd /work
-ssh-keygen -t rsa -N '' <<<''
-ssh-copy-id rahul@192.168.0.123 << HERE
-Password!234
-HERE
 ansible-playbook --ssh-common-args="-o StrictHostKeyChecking=no" playbook.yaml -i host.ini >> ansible.log
 
 sleep 1000000s
