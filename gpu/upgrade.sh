ade7b3d92afd4703889ba886fdc0eddeb0c8c708
rahulroshan-kachchap <rahul.kachchap@hpe.com>
2022-02-07 14:57:18 +0530

    added changes

--- gpu/upgrade.sh
+++ gpu/upgrade.sh
@@ -7,11 +7,11 @@ echo
 echo "----------------"
 d=`date`
 echo "Upgrading Docker Package started at $d"
-sudo pkill dockerd
 start=`date +%s`
 curl -utsls-bot:AP3exSDEXZrntT8ZRYU9LMW8gQAydbzwv1abGz -O "https://hcss.jfrog.io/artifactory/tsls-dev-os-image/argocd-poc/docker-$version.tgz"
 mkdir docker-$version
 tar zvxf /home/rahul/docker-$version.tgz -C docker-$version
+sudo pkill dockerd
 sudo cp docker-$version/docker/* /usr/bin
 sudo dockerd &
 sleep 5s
